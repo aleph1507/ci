bb08637207eeec20541fcaa0e6ced335d98c1ad0
Aleksandar Hristovski <xrristo@gmail.com>
2017-12-20 19:16:15 +0100

    ci00

---
+++ application/controllers/magazine.php
@@ -0,0 +1,11 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Magazine extends CI_Controller {
+	/***
+	*   index page for the magazine controller
+	***/
+	public function index(){
+		echo '<h2>My magazines</h2>';
+	}
+}
